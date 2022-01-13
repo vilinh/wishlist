92792645f5f40224c2dc82277934be242a4c01d9
Vi-Linh Vu <vi-linhvu@Vi-Linhs-MacBook-Pro.local>
2022-01-12 21:01:52 -0800

    context api

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home";
+import { GlobalProvider } from "./context/GlobalContext";
 const API_BASE = "http://localhost:3001";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Home />
+      <GlobalProvider>
+        <Navbar />
+        <Home />
+      </GlobalProvider>
     </div>
   );
 }
--- client/src/context/GlobalContext.js
+++ client/src/context/GlobalContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useReducer, useEffect } from "react";
-
+import axios from "axios";
 
 // initial state
 const initialState = {
@@ -12,6 +12,25 @@ const initialState = {
 // reducer
 const globalReducer = (state, action) => {
   switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.payload,
+        fetchingUser: false,
+      };
+    case "SET_WISHLIST":
+      return {
+        ...state,
+        wishList: action.payload,
+      };
+    case "RESET_USER":
+      return {
+        ...state,
+        user: null,
+        wishList: [],
+        boughtWishlist: [],
+        fetchingUser: false,
+      };
     default:
       return state;
   }
@@ -22,21 +41,45 @@ export const GlobalContext = createContext(initialState);
 
 // provider
 export const GlobalProvider = (props) => {
-    const [state, dispatch] = useReducer(globalReducer, initialState);
+  const [state, dispatch] = useReducer(globalReducer, initialState);
 
-    // action: get current user
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
 
-    const value = {
-        ...state,
+  // action: get current user
+  const getCurrentUser = async () => {
+    try {
+      const res = await axios.get("/auth/current");
+
+      if (res.data) {
+        const itemRes = await axios.get("/items");
+
+        if (itemRes.data) {
+          dispatch({ type: "SET_USER", payload: res.data });
+          dispatch({ type: "SET_WISHLIST", payload: itemRes.data.items });
+        }
+      } else {
+        dispatch({ type: "RESET_USER" });
+      }
+    } catch (err) {
+      console.log(err);
+      dispatch({ type: "RESET_USER" });
     }
+  };
 
-    return (
-        <GlobalContext.Provider value={value}>
-            {props.children}
-        </GlobalContext.Provider>
-    )
-}
+  const value = {
+    ...state,
+    getCurrentUser,
+  };
+
+  return (
+    <GlobalContext.Provider value={value}>
+      {props.children}
+    </GlobalContext.Provider>
+  );
+};
 
 export function useGlobalContext() {
-    return useContext(GlobalContext);
+  return useContext(GlobalContext);
 }
